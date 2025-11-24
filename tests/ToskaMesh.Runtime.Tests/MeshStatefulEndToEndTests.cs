896bd0e8f865688afee7fe5d40b39fa4ba3c91b7
Jason Vowell <jason@nullsync.com>
2025-11-23 23:53:33 -0600

    Add Orleans stateful host sample/test for MeshServiceHost

---
+++ tests/ToskaMesh.Runtime.Tests/MeshStatefulEndToEndTests.cs
@@ -0,0 +1,43 @@
+using Microsoft.Extensions.DependencyInjection;
+using Orleans;
+using ToskaMesh.Runtime;
+using ToskaMesh.Runtime.Orleans;
+using ToskaMesh.Runtime.Tests.StatefulSample;
+using Xunit;
+
+namespace ToskaMesh.Runtime.Tests;
+
+public class MeshStatefulEndToEndTests
+{
+    [Fact]
+    public async Task RunStatefulAsync_hosts_grain_and_serves_requests()
+    {
+        using var host = MeshServiceHost.StartStateful(
+            configureSilo: silo =>
+            {
+                silo.ServiceName = "stateful-e2e";
+                silo.SiloPort = 21112;
+                silo.GatewayPort = 21001;
+                silo.ClusteringMode = "localhost";
+            },
+            configureOptions: options =>
+            {
+                options.ServiceName = "stateful-e2e";
+                options.RegisterAutomatically = false;
+                options.HeartbeatEnabled = false;
+                options.AllowNoopServiceRegistry = true;
+            },
+            configureServices: services =>
+            {
+                services.AddSingleton<ITestGrain, TestGrain>();
+            });
+
+        var client = host.Services.GetRequiredService<IGrainFactory>();
+        var grain = client.GetGrain<ITestGrain>("abc");
+        var result = await grain.SayHello("world");
+
+        Assert.Equal("hello world", result);
+
+        await host.StopAsync();
+    }
+}
